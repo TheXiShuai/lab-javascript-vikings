e903945432fcaf9c1a3f979c99a3ba28cabcbc88
John Doe <johndoe@example.com>
2023-07-16 15:18:41 +0200

    half done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,88 @@
 // Soldier
-class Soldier {}
+class Soldier  {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+       this.health -=  damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength) {
+        super( health, strength)
+        this.name = name
+        
+    }
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+        if (this.health <= 0) {
+        return `${this.name} has died in act of combat`;
+        }
+        return `${this.name} has received ${damage} points of damage`;
+        }
+    battleCry(){
+       return "Odin Owns You All!"
+    }
+   
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+        if (this.health <= 0) {
+        return "A Saxon has died in combat";
+        }
+        return `A Saxon has received ${damage} points of damage`;
+      }
+}
+
+
 
 // War
-class War {}
+class War  {
+constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    }
+    addViking(Viking){
+        this.vikingArmy.push(viking);
+        
+    }
+    addSaxon(Saxon){
+        this.saxonArmy.push(saxon);
+       
+    }
+    vikingAttack() {
+    
+      }
+    saxonAttack() {
+     
+      }
+    showStatus() {
+        if (this.saxonArmy.length === 0) {
+          return "Vikings have won the war of the century!";
+        }
+        if (this.vikingArmy.length === 0) {
+          return "Saxons have fought for their lives and survive another day...";
+        }
+        if (this.saxonArmy.length === 1 && this.vikingArmy.length === 1) {
+          return "Vikings and Saxons are still in the thick of battle.";
+        }
+      }
+    }
+
+
+
+
+
+
+
+
